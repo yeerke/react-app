bd1611dc0e61f932956b6f7c464e74ff122f1024
yerke26 <yerkebulan@nethermind.io>
2025-04-27 00:47:48 +0500

    asdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import UserPage from './pages/UserPage';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <main className="app-main">
+          <div className="app-content">
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/user/:userId" element={<UserPage />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 }
-
-export default App;
